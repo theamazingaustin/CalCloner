073446b3404d52a4ca86f7398b89037944a8944c
Austin Tompkins <theamazingaustin@gmail.com>
2026-09-14 07:39:59 -0400

    build: configure debug build variant as installable parallel Beta APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,10 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".beta"
+            versionNameSuffix = "-beta"
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
